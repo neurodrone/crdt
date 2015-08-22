41c54df2fbac545ad6aef6ef4cba5525e7944495
Vaibhav Bhembre <vaibhav@bhembre.com>
2015-08-22 17:58:57 -0400

    Add documentation for G-Counter and PN-Counter

--- g_counter.go
+++ g_counter.go
@@ -2,11 +2,21 @@ package crdt
 
 import "github.com/satori/go.uuid"
 
+// GCounter represent a G-counter in CRDT, which is
+// a state-based grow-only counter that only supports
+// increments.
 type GCounter struct {
-	ident   string
+	// ident provides a unique identity to each replica.
+	ident string
+
+	// counter maps identity of each replica to their
+	// entry values i.e. the counter value they individually
+	// have.
 	counter map[string]int
 }
 
+// NewGCounter returns a *GCounter by pre-assigning a unique
+// identity to it.
 func NewGCounter() *GCounter {
 	return &GCounter{
 		ident:   uuid.NewV4().String(),
@@ -14,10 +24,15 @@ func NewGCounter() *GCounter {
 	}
 }
 
+// Inc increments the GCounter by the value of 1 everytime it
+// is called.
 func (g *GCounter) Inc() {
 	g.IncVal(1)
 }
 
+// IncVal allows passing in an arbitrary delta to increment the
+// current value of counter by. Only positive values are accepted.
+// If a negative value is provided the implementation will panic.
 func (g *GCounter) IncVal(incr int) {
 	if incr < 0 {
 		panic("cannot decrement a gcounter")
@@ -25,6 +40,8 @@ func (g *GCounter) IncVal(incr int) {
 	g.counter[g.ident] += incr
 }
 
+// Count returns the total count of this counter across all the
+// present replicas.
 func (g *GCounter) Count() (total int) {
 	for _, val := range g.counter {
 		total += val
@@ -32,6 +49,10 @@ func (g *GCounter) Count() (total int) {
 	return
 }
 
+// Merge combines the counter values across multiple replicas.
+// The property of idempotency is preserved here across
+// multiple merges as when no state is changed across any replicas,
+// the result should be exactly the same everytime.
 func (g *GCounter) Merge(c *GCounter) {
 	for ident, val := range c.counter {
 		if v, ok := g.counter[ident]; !ok || v < val {
--- pn_counter.go
+++ pn_counter.go
@@ -1,10 +1,15 @@
 package crdt
 
+// PNCounter represents a state-based PN-Counter. It is
+// implemented as sets of two G-Counters, one that tracks
+// increments while the other decrements.
 type PNCounter struct {
 	pCounter *GCounter
 	nCounter *GCounter
 }
 
+// NewPNCounter returns a new *PNCounter with both its
+// G-Counters initialized.
 func NewPNCounter() *PNCounter {
 	return &PNCounter{
 		pCounter: NewGCounter(),
@@ -12,26 +17,46 @@ func NewPNCounter() *PNCounter {
 	}
 }
 
+// Inc monotonically increments the current value of the
+// PN-Counter by one.
 func (pn *PNCounter) Inc() {
 	pn.IncVal(1)
 }
 
+// IncVal increments the current value of the PN-Counter
+// by the delta incr that is provided. The value of delta
+// has to be >= 0. If the value of delta is < 0, then this
+// implementation panics.
 func (pn *PNCounter) IncVal(incr int) {
 	pn.pCounter.IncVal(incr)
 }
 
+// Dec monotonically decrements the current value of the
+// PN-Counter by one.
 func (pn *PNCounter) Dec() {
 	pn.DecVal(1)
 }
 
+// DecVal adds a decrement to the current value of
+// PN-Counter by the value of delta decr. Similar to
+// IncVal, the value of decr cannot be less than 0.
 func (pn *PNCounter) DecVal(decr int) {
 	pn.nCounter.IncVal(decr)
 }
 
+// Count returns the current value of the counter. It
+// subtracts the value of negative G-Counter from the
+// positive grow-only counter and returns the result.
+// Because this counter can grow in either direction,
+// negative integers as results are possible.
 func (pn *PNCounter) Count() int {
 	return pn.pCounter.Count() - pn.nCounter.Count()
 }
 
+// Merge combines both the PN-Counters and saves the result
+// in the invoking counter. Respective G-Counters are merged
+// i.e. +ve with +ve and -ve with -ve, but not computation
+// is actually performed.
 func (pn *PNCounter) Merge(pnpn *PNCounter) {
 	pn.pCounter.Merge(pnpn.pCounter)
 	pn.nCounter.Merge(pnpn.nCounter)
